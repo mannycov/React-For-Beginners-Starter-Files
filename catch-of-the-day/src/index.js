6237c9b297add1eae385dd5f82b502759b428865
Emanuel <manny.cov09@gmail.com>
2017-12-06 16:03:19 -0800

    Add Header, Inventory, Order, and parent App component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,7 @@
 // let's go!
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './css/style.css';
+import App from './components/App';
+
+render(<App />, document.querySelector('#main'));
