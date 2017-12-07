2783456495af2b1aace11a26e24d9349cb4b8e15
Emanuel <manny.cov09@gmail.com>
2017-12-07 00:30:51 -0800

    add input to form and add goToStore function

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,29 @@
 // let's go!
 import React, { Component } from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  }
+
+  goToStore(e) {
+    e.preventDefault();
+    console.log('go to store invoked')
+
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
       <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input 
+          type="text" 
+          required placeholder="Store Name" 
+          defaultValue={getFunName()}
+          ref={(input) => { this.storeInput = input }}
+          />
         <button type="submit">Visit Store -></button>
       </form>
     )
