c19b107f065e7c2f262316b212b64e7224fbc56b
Emanuel <manny.cov09@gmail.com>
2017-12-10 18:54:11 -0800

    add component to render individual fish

---
+++ catch-of-the-day/src/components/Fish.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Fish extends Component {
+  render() {
+    // const details = this.props.details;
+    // es6 destructuring below
+    const { details } = this.props;
+    return (
+      <li className="menu-fish">
+        <img src={details.image} alt={details.name}/>
+        <h3 className="fish-name">
+          {details.name}
+          <span className="price">{details.price}</span>
+        </h3>
+        <p>{details.desc}</p>
+        <button>Add to Order</button>
+      </li>
+
+    )
+  }
+} 
+
+export default Fish;
