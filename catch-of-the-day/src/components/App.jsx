d05b3b96f34123bc94fbddc38bf8c87704e79a4b
Emanuel <manny.cov09@gmail.com>
2017-12-11 17:11:48 -0800

    update Order component to render individual fish with price and amount

--- catch-of-the-day/src/components/App.jsx
+++ catch-of-the-day/src/components/App.jsx
@@ -65,7 +65,10 @@ class App extends Component {
             }
           </ul>
         </div>
-        <Order />
+        <Order 
+          fishes={this.state.fishes}
+          order={this.state.order}
+        />
         <Inventory 
           addFish={this.addFish}
           loadSamples={this.loadSamples} 
